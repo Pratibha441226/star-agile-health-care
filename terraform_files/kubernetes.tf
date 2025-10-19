0f2ac6b6fd24cb3edfaeab85352908ae2c55772b
Pratibha441226 <pradhan.pratibha12@gmail.com>
2025-10-19 10:24:39 +0530

    Update kubernetes.tf

--- terraform_files/kubernetes.tf
+++ terraform_files/kubernetes.tf
@@ -10,4 +10,30 @@ key_name = "mykey"
    volume_size = 20
    volume_type = "gp2"
 }
-tages
+tags = {
+     name = "kubernetes-server"
+}
+provisioner "remote-exec" {
+inline = [
+"sudo apt update -y",
+"sudo apt install docker.io -y",
+"sudo systemctl start docker",
+"sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+"sudo chmod +x /home/ubuntu/minikube-linux-amd64",
+"sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+"curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/stable.txt`/bin/linux/amd64/kubectl",
+"sudo chmod +x /home/ubuntu/kubectl",
+"sudo cp kubectl /usr/local/bin/kubectl",
+"sudo group docker",
+"sudo usermod -aG docker ubuntu",
+]
+connection {
+type = "ssh"
+host = self.public_ip
+user = "ubuntu"
+private_key = file("./mykey.pem")
+}
+}
+}
+
+
