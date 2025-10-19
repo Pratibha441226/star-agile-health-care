fcbd559a12047a65392d53dd7b7adc145bcabb96
Pratibha441226 <pradhan.pratibha12@gmail.com>
2025-10-19 13:00:43 +0530

    Update kubernetes.tf

--- terraform_files/kubernetes.tf
+++ terraform_files/kubernetes.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 resource "aws_instance" "kubernetes-server" {
 ami =""
 instance_type = "t3.medium"
-vpc_seurity_group_ids =[""]
+vpc_seurity_group_ids =["sg-0e572ed58dbc9ec7c"]
 key_name = "mykey"
  root_block_device {
    volume_size = 20
